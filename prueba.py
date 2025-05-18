8239d74e31f81b66f47277573616a83cbb4b85b9
SantiagoVelasquezO <santiagovelasquezorozco17@gmail.com>
2025-05-17 23:25:19 -0500

    S y S como parámetros

--- prueba.py
+++ prueba.py
@@ -4,10 +4,17 @@ def cuadrado(x,y):
 
 print(cuadrado(5,8))
 
-def paseosantafe(som,svo):
+# def paseosantafe(som,svo):
+#     return som * svo
+
+# print(paseosantafe(16,30))
+
+int = som = 16
+int = svo = 30
+def paseosantafe2(som,svo):
     return som * svo
 
-print(paseosantafe(16,30))
+print(paseosantafe2(som,svo))
 
 # <div class="row">
 #             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, esse accusantium? Dolorem, ab possimus. Omnis delectus beatae, consequuntur fugit accusantium quibusdam odit at vel placeat est nemo, commodi nam quaerat.</p>
