c52cd6a35a505debcb04645e66f8b077117f9077
SantiagoVelasquezO <santiagovelasquezorozco17@gmail.com>
2025-05-17 23:21:32 -0500

    direccional

--- prueba.py
+++ prueba.py
@@ -4,6 +4,11 @@ def cuadrado(x,y):
 
 print(cuadrado(5,8))
 
+def paseosantafe(som,svo):
+    return som * svo
+
+print(paseosantafe(16,30))
+
 # <div class="row">
 #             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, esse accusantium? Dolorem, ab possimus. Omnis delectus beatae, consequuntur fugit accusantium quibusdam odit at vel placeat est nemo, commodi nam quaerat.</p>
 #         </div>
