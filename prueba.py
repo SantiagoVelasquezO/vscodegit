870afee04b4bebc0ec1a9ae2b4717dde51d1018f
SantiagoVelasquezO <santiagovelasquezorozco17@gmail.com>
2025-05-17 23:15:36 -0500

    paseo santafe

--- prueba.py
+++ prueba.py
@@ -3,3 +3,10 @@ def cuadrado(x,y):
 
 
 print(cuadrado(5,8))
+
+# <div class="row">
+#             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, esse accusantium? Dolorem, ab possimus. Omnis delectus beatae, consequuntur fugit accusantium quibusdam odit at vel placeat est nemo, commodi nam quaerat.</p>
+#         </div>
+# <div class="row">
+#             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, esse accusantium? Dolorem, ab possimus. Omnis delectus beatae, consequuntur fugit accusantium quibusdam odit at vel placeat est nemo, commodi nam quaerat.</p>
+#         </div>
